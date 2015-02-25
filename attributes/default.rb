064e3a78e6be0e0812274ac1df8a76cde67b8b77
Jing Nghik <jnghik@gmail.com>
2015-02-25 09:52:28 -0600

    changed agent defaults to host

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,7 +5,7 @@ default["alertlogic"]["agent"]["pkg_base_url"] = "https://scc.alertlogic.net/sof
 default["alertlogic"]["agent"]["pkg_vsn"]["deb"] = "_LATEST_"
 default["alertlogic"]["agent"]["pkg_vsn"]["rpm"] = "-LATEST-1."
 default["alertlogic"]["agent"]["controller_host"] = "vaporator.alertlogic.com"
-default["alertlogic"]["agent"]["inst_type"] = nil
+default["alertlogic"]["agent"]["inst_type"] = "host"
 default["alertlogic"]["agent"]["firewall"] = [
     # allow destination "network/mask:port"
     "204.110.218.96/27:443",
