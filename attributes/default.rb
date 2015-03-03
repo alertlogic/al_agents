d00ba6346640b565319ddb66a2f08a4c8bd7ddaa
Harvey Bendana <harvey.bendana@nordstrom.com>
2015-03-02 16:02:43 -0800

    remove trailing . from attribute

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,7 +4,7 @@
 default["alertlogic"]["agent"]["pkg_base_url"] = "https://scc.alertlogic.net/software"
 default["alertlogic"]["agent"]["pkg_vsn"]["debian"] = "_LATEST_"
 default["alertlogic"]["agent"]["pkg_vsn"]["rhel"] = "-LATEST-1."
-default["alertlogic"]["agent"]["pkg_vsn"]["windows"] = "-LATEST."
+default["alertlogic"]["agent"]["pkg_vsn"]["windows"] = "-LATEST"
 default["alertlogic"]["agent"]["controller_host"] = nil
 default["alertlogic"]["agent"]["inst_type"] = nil
 default["alertlogic"]["agent"]["provision_key"] = nil
