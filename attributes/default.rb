a1b31eba4840d7693fa171429924e9851b63293a
Harvey Bendana <harvey.bendana@nordstrom.com>
2015-03-04 15:19:38 -0800

    add back in default attribute for iptables

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,5 +7,10 @@ default["alertlogic"]["agent"]["pkg_vsn"]["rhel"] = "-LATEST-1."
 default["alertlogic"]["agent"]["pkg_vsn"]["windows"] = "-LATEST"
 default["alertlogic"]["agent"]["controller_host"] = nil
 default["alertlogic"]["agent"]["inst_type"] = nil
+default["alertlogic"]["agent"]["firewall"] = [
+    # allow destination "network/mask:port"
+    "204.110.218.96/27:443",
+    "185.54.124.96/27:443"
+]
 default["alertlogic"]["agent"]["provision_key"] = nil
 default["alertlogic"]["agent"]["use_proxy"] = nil
