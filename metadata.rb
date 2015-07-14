51df2d7f565174d44dc244e9c583adf48706dab2
john ramos <john.ramos@gmail.com>
2015-07-14 15:44:53 -0700

    bumping version

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ issues_url 'https://github.com/dualspark/al-agent/issues'
 license 'All rights reserved'
 description 'Installs/Configures the Alert Logic Agent'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.3'
+version '1.0.0'
 
 depends 'selinux_policy'
 depends 'rsyslog'
