a08589ad4e5391ce4eea8e0954d4f3df03c3eb6f
Stephen Saine <sainejob@users.noreply.github.com>
2016-05-11 15:32:33 -0700

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
 name 'al_agents'
 maintainer 'Justin Early'
 maintainer_email 'jearly@alertlogic.com'
-source_url 'https://github.com/alertlogic/al_agents'
-issues_url 'https://github.com/alertlogic/al_agents/issues'
+source_url 'https://github.com/alertlogic/al_agents' if respond_to?(:source_url)
+issues_url 'https://github.com/alertlogic/al_agents/issues' if respond_to?(:issues_url)
 license 'Apache 2.0 License'
 description 'Installs/Configures the Alert Logic Agent'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
