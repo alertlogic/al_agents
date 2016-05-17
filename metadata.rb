2a9923b3fa93f8514395a3894df50c57fae1d2dc
Justin Early <jearly@Latitude-E5420.local>
2016-05-16 20:23:46 -0500

    restoring respond_to feature from commit a08589a

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ issues_url 'https://github.com/alertlogic/al_agents/issues' if respond_to?(:issu
 license 'Apache 2.0 License'
 description 'Installs/Configures the Alert Logic Agent'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '1.3.3'
+version '1.3.4'
 
 depends 'selinux_policy'
 depends 'rsyslog', '= 2.2.0'
