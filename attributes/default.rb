f1dc8846f6ad10c7c17cdb353cde2f71698fd03f
Andres F Vargas <andphe@gmail.com>
2016-07-25 11:20:20 -0500

    Make package repository configurable
    so you can have sets of agents per environment using different repo urls.

--- attributes/default.rb
+++ attributes/default.rb
@@ -12,6 +12,8 @@ default['al_agents']['package']['name'] = 'al-agent'
 
 default['al_agents']['agent']['ignore_failure'] = false
 
+default['al_agents']['package']['base_url'] = 'https://scc.alertlogic.net/software/'
+
 case node['platform_family']
 when 'rhel', 'fedora'
   default['al_agents']['agent']['al_agent_service'] = 'al-agent'
@@ -21,21 +23,21 @@ when 'rhel', 'fedora'
     default['al_agents']['syslog_ng']['source_log'] = 's_sys'
   end
   if node['kernel']['machine'] == 'x86_64'
-    default['al_agents']['package']['url'] = 'https://scc.alertlogic.net/software/al-agent-LATEST-1.x86_64.rpm'
+    default['al_agents']['package']['url'] = "#{node['al_agents']['package']['base_url']}al-agent-LATEST-1.x86_64.rpm"
   else
-    default['al_agents']['package']['url'] = 'https://scc.alertlogic.net/software/al-agent-LATEST-1.i386.rpm'
+    default['al_agents']['package']['url'] = "#{node['al_agents']['package']['base_url']}al-agent-LATEST-1.i386.rpm"
   end
 when 'debian'
   default['al_agents']['agent']['al_agent_service'] = 'al-agent'
   default['al_agents']['syslog_ng']['source_log'] = 's_src'
   if node['kernel']['machine'] == 'x86_64'
-    default['al_agents']['package']['url'] = 'https://scc.alertlogic.net/software/al-agent_LATEST_amd64.deb'
+    default['al_agents']['package']['url'] = "#{node['al_agents']['package']['base_url']}al-agent_LATEST_amd64.deb"
   else
-    default['al_agents']['package']['url'] = 'https://scc.alertlogic.net/software/al-agent_LATEST_i386.deb'
+    default['al_agents']['package']['url'] = "#{node['al_agents']['package']['base_url']}al-agent_LATEST_i386.deb"
   end
 when 'windows'
   default['al_agents']['agent']['al_agent_service'] = 'al_agent'
-  default['al_agents']['package']['url'] = 'https://scc.alertlogic.net/software/al_agent-LATEST.msi'
+  default['al_agents']['package']['url'] = "#{node['al_agents']['package']['base_url']}al_agent-LATEST.msi"
   if node['kernel']['machine'] == 'x86_64'
     default['al_agents']['windows_install_guard'] = 'C:\Program Files (x86)\Common Files\AlertLogic\host_key.pem'
   else
