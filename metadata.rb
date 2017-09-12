13e2ec06cdf67d9caceaf3d70865f902af746291
username-is-already-taken2 <gary.bright@niu-solutions.com>
2017-09-12 10:05:31 +0100

    Resolved FC069 & FC078

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Justin Early'
 maintainer_email 'jearly@alertlogic.com'
 source_url 'https://github.com/alertlogic/al_agents' if respond_to?(:source_url)
 issues_url 'https://github.com/alertlogic/al_agents/issues' if respond_to?(:issues_url)
-license 'Apache 2.0 License'
+license 'Apache-2.0'
 description 'Installs/Configures the Alert Logic Agent'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '1.3.6'
