e76e8eb9f655a92a5fced9ef104e7d1be1ceaee0
username-is-already-taken2 <gary.bright@niu-solutions.com>
2017-09-12 10:31:36 +0100

    Changed default egress protocol to https

--- libraries/egress.rb
+++ libraries/egress.rb
@@ -27,7 +27,7 @@ class Chef
 
       def schemed_url
         schemed = url
-        schemed = "https://#{url}" unless url =~ %r{^http:\/\/}i
+        schemed = "https://#{url}" unless url =~ %r{^http:\/\/}i || url =~ %r{^https:\/\/}i
         schemed
       end
 
